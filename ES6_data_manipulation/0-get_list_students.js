1d07d0298a48580973ce00bd8e53979dbb746923
Matross-20 <matrossmthawelanga@gmail.com>
2024-07-28 12:54:06 +0200

    Basic list of objects

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,11 @@
 function getListStudents() {
-    const studentObj = [
-      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-      { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    ];
-  
-    return studentObj;
-  }
-  
-  export default getListStudents;
+  const studentObj = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return studentObj;
+}
+
+export default getListStudents;
