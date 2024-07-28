85fcd348b15e62ebd0a026dfeaa32681f1f04c35
Matross-20 <matrossmthawelanga@gmail.com>
2024-07-28 12:58:13 +0200

    More mapping

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,8 @@
 export default function getListStudentIds(arr) {
-    if (!Array.isArray(arr)) {
-        const emptyArray = []
-        return emptyArray
-    }
-    const studentsArr = arr.map(item => item.id)
-    return studentsArr
+  if (!Array.isArray(arr)) {
+    const emptyArray = [];
+    return emptyArray;
+  }
+  const studentsArr = arr.map((item) => item.id);
+  return studentsArr;
 }
