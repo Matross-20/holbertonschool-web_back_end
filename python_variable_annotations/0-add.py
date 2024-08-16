696b42e1ab03ab6eca6617fb717a02b46996daa0
Matross-20 <matrossmthawelanga@gmail.com>
2024-08-16 16:26:13 +0200

    Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+        returns the sum of a and b
+
+        Args:
+            a (float): number
+            b (float): number
+
+        Returns:
+            the sum of a and b
