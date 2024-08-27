5461ceca1e1ad0978abc55d27bdab7c05d1c9b3c
Matross-20 <matrossmthawelanga@gmail.com>
2024-08-27 17:51:34 +0200

    Tasks

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python3
 
-"""
-    similar function to wait_n but calls task_wait random instead
-    Imports:
-        asyncio
-        typing
-        task_wait_random
-"""
+"""Module contains function that takes two integers
 
-import asyncio
+Imports:
+    typing: module for list type annotation
+    random_wait: function delays for n seconds and returns n
+"""
 from typing import List
-
-task_wait_random = __import__('3-tasks').task_wait_random
+random_wait = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Waits for n seconds, then returns a list of n floats representing the time waited for each iteration.
+    
+    Args:
+        n (int): The number of times to wait and record the time.
+        max_delay (int): The maximum number of seconds to wait for each iteration.
+    
+    Returns:
+        List[float]: A list of n floats representing the time waited for each iteration.
     """
-        Returns a list of all the delays (float values)
-        by calling task_wait_random n times.
-        Args:
-            n (int): Number of times to call task_wait_random.
-            max_delay (int): Maximum delay value for each call.
-        Returns:
-            List[float]: A list of delays obtained from task_wait_random.
-    """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    return delays
+        
+    my_list: List[float] = []
+    i: int = 0
+
+    while i < n:
+        result = await random_wait(max_delay)
+        my_list.append(result)
+        i += 1
+
+    for end in range(len(my_list), 1, -1):
+        for j in range(1, end):
+            if my_list[j - 1] > my_list[j]:
+                my_list[j - 1], my_list[j] = my_list[j], my_list[j - 1]
+    return my_list
