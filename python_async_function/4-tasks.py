120c98d7a3995616d8ce157395889f7d30c0b2a6
Matross-20 <matrossmthawelanga@gmail.com>
2024-08-27 17:56:54 +0200

    Tasks

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,36 +1,30 @@
 #!/usr/bin/env python3
-
-"""Module contains function that takes two integers
-
-Imports:
-    typing: module for list type annotation
-    random_wait: function delays for n seconds and returns n
-"""
+""" Tasks """
+import asyncio
+import random
 from typing import List
-random_wait = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Waits for n seconds, then returns a list of n floats representing the time waited for each iteration.
-    
-    Args:
-        n (int): The number of times to wait and record the time.
-        max_delay (int): The maximum number of seconds to wait for each iteration.
-    
-    Returns:
-        List[float]: A list of n floats representing the time waited for each iteration.
+task_wait_random = __import__('3-tasks').wait_random
+
+
+async def task_wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """
-        
-    my_list: List[float] = []
-    i: int = 0
+        Args:
+            max_delay: max wait
+            n: spawn function
 
-    while i < n:
-        result = await random_wait(max_delay)
-        my_list.append(result)
-        i += 1
+        Return:
+            multiples tasks
+    """
+    delays: List[float] = []
+    tasks: List[asyncio.Task] = []
 
-    for end in range(len(my_list), 1, -1):
-        for j in range(1, end):
-            if my_list[j - 1] > my_list[j]:
-                my_list[j - 1], my_list[j] = my_list[j], my_list[j - 1]
-    return my_list
+    for _ in range(n):
+        tasks.append(task_wait_random(max_delay))
+
+    for task in asyncio.as_completed((tasks)):
+        delay = await task
+        delays.append(delay)
+
+    return delays
