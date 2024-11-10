4c4fc7ac1125a4580a1bdbce8b45abb34fcb536a
Matross-20 <matrossmthawelanga@gmail.com>
2024-11-10 15:49:40 +0200

    Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,14 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("data", (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  
-  // Delay the exit to allow the output to be printed first
-  setTimeout(() => process.exit(), 0);
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-process.on("exit", () => {
-  console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
