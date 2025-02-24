1852ba973837c5e7a2ceee12194baf16e8b4b6cc
Matross-20 <matrossmthawelanga@gmail.com>
2025-02-24 12:39:14 +0200

    User model

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -6,10 +6,10 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class User(base)
+class User(Base):
     """Representation of a user"""
     __tablename__ = 'users'
-    id = Column(Integer, primary keys=True)
+    id = Column(Integer, primary_key=True)
     email= Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
