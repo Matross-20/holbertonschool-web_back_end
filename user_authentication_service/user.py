14097732513472ece3812c4906dd4b2f7ba627e0
Matross-20 <matrossmthawelanga@gmail.com>
2025-03-02 11:29:26 +0200

    User authentication service

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -7,10 +7,10 @@ Base = declarative_base()
 
 
 class User(Base):
-    """Representation of a user"""
+    """Representation of a user """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email= Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
     reset_token = Column(String(250))
