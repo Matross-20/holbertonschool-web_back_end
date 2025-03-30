ff3c59e96d2966fb4e6025c11929785496b4b9a1
Matross-20 <matrossmthawelanga@gmail.com>
2025-03-30 17:33:26 +0200

    Unittests in JS

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -1,32 +1,29 @@
+const mocha = require('mocha');
+const { expect } = require('chai');
 const sinon = require('sinon');
-var { expect } = require('chai');
 
 const sendPaymentRequestToApi = require('./5-payment');
-const Utils = require('./utils');
 
-describe('Hooks', function () {
-  let spyConsole;
-  beforeEach(function () {
-    // runs before each test in this block
-    spyConsole = sinon.spy(console, 'log');
+describe('sendPaymentRequestToApi', () => {
+  let spy;
+
+  beforeEach(() => {
+    spy = sinon.spy(console, 'log');
   });
 
-  afterEach(function () {
-    // runs after each test in this block
-    spyConsole.restore();
+  afterEach(() => {
+    spy.restore();
   });
 
-  it('logs correctly with 100, 20', () => {
+  it('should log 120 if a = 100 and b = 20', () => {
     sendPaymentRequestToApi(100, 20);
-
-    expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
-    expect(spyConsole.calledOnce).to.be.true;
+    expect(spy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(spy.calledOnce).to.be.true;
   });
 
-  it('logs correctly with 10, 10', () => {
+  it('should log 20 if a = 10 and b = 10', () => {
     sendPaymentRequestToApi(10, 10);
-
-    expect(spyConsole.calledOnceWithExactly('The total is: 20')).to.be.true;
-    expect(spyConsole.calledOnce).to.be.true;
+    expect(spy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(spy.calledOnce).to.be.true;
   });
 });
